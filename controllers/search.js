8c4283e0f22ffc87087ff5aa04ec6a8b177ae4fc
Brandon Aray <alex.aray94@gmail.com>
2023-02-18 21:18:48 -0500

    Added filtering logic to search endpoint controller using regEx

--- controllers/search.js
+++ controllers/search.js
@@ -1,16 +1,28 @@
+const User = require('../models/User')
+const Medic = require('../models/Medic')
+const Hospital = require('../models/Hospital')
+
+const MODELS = [User, Medic, Hospital]
+
 const getSearch = async (req, res) => {
 	const { search } = req.params
 	try {
-		if(!search) {
-			return res.status(400).json({
-				msg: 'Por favor ingrese un valor'
-			})
-		}
+	  const regEx = new RegExp(search, 'i')
 
+		// const results = await Promise.all(
+		// 	MODELS.map(model => model.find({ nombre: regEx }))
+		// )
+		const [users, medics, hospitals] = await Promise.all(
+			User.find({ nombre: regEx }),
+			Medic.find({ nombre: regEx }),
+			Hospital.find({ nombre: regEx })
+		)
 
 	  res.status(200).json({
 		  msg: 'Ruta de busqueda',
-		  searchVal: search
+		  users,
+		  medics,
+		  hospitals
 	  })
 	} catch (err) {
 		res.status(500).json({
