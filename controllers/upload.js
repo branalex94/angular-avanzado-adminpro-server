e0b44e3f03c4ffe20df5e897ce21117a17da49b8
Brandon Aray <alex.aray94@gmail.com>
2023-02-20 16:41:16 -0500

    Added file upload functionality to controller as well as file uploads directory

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,3 +1,5 @@
+const { v4: uuidv4 } = require('uuid')
+
 const fileUpload = async (req, res) => {
 	const { table, id } = req.params
 
@@ -10,15 +12,44 @@ const fileUpload = async (req, res) => {
 	}
 
   // Validar si algun archivo fue enviado
-	if(!req.files || Object.keys(req.files).length  === 0) {
+	if(!req.files || Object.keys(req.files).length === 0) {
 		return res.status(400).json({
 			msg: 'No hay imagenes enviadas'
 		})
 	}
 
-	res.json({
-		msg: 'Ruta de subir archivos!'
-	})
+	const { image } = req.files
+	const splitFilename = image.name.split('.')
+  const fileExt = splitFilename[splitFilename.length - 1]
+
+  const validFileExtensions = ['png', 'jpg', 'jpeg', 'gif']
+
+  if(!validFileExtensions.includes(fileExt)) {
+  	return res.status(400).json({
+			msg: 'Tipo de archivo no valido'
+		})
+  }
+
+  // Generar nombre del archivo
+  const newFilename = `${uuidv4()}.${fileExt}`
+
+  // Ruta para guardar img
+  const path = `./uploads/${table}/${newFilename}`
+
+  image.mv(path, (err) => {
+  	if(err) {
+  		return res.status(500).json({
+  			msg: 'Hubo un error subiendo tu archivo'
+  		})
+  	}
+
+		res.json({
+			msg: 'Ruta de subir archivos!',
+			newFilename
+		})
+  })
+
+
 }
 
 module.exports = {
